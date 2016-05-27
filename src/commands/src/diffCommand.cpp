1e6ebfb1a7ba8c99db2a2a9180fb843069d929a4
Emanuel Bringas <emab73@gmail.com>
2016-05-27 17:33:55 -0300

    Now diff command detect when  the two execution differs.

--- src/commands/src/diffCommand.cpp
+++ src/commands/src/diffCommand.cpp
@@ -37,42 +37,41 @@ void DiffCommand::execute(const Arguments& args, NSDebuggingContext::Context& ct
     auto& instance2 = ctx.getInstance(mili::from_string<NSCommon::InstanceId>(args[InstanceNumber2]));
     mili::assert_throw<NSCommon::InstancesAreNotStopped>(not instance1.isRunning() and not instance2.isRunning());
 
-    std::cout << instance1.frame() << std::endl;
-    instance1.next();
-    sleep(1);
-    std::cout << instance1.frame() << std::endl;
-    instance1.next();
-    std::cout << instance1.frame() << std::endl;
-    instance1.next();
+    auto instancesAlive = instance1.isAlive() and instance2.isAlive();
+    auto f1 = instance1.frame();
+    auto f2 = instance2.frame();
+    auto framesAreEquals = (f1 == f2);
+    while (instancesAlive and framesAreEquals)
+    {
+        const auto future1 = instance1.next();
+        const auto future2 = instance2.next();
+        future1.wait();
+        future2.wait();
+        instancesAlive = instance1.isAlive() and instance2.isAlive();
+        if (instancesAlive)
+        {
+            f1 = instance1.frame();
+            f2 = instance2.frame();
+            framesAreEquals = (f1 == f2);
+        }
+    }
 
-    // while ((instance1.isRunning() and instance2.isRunning()) and (instance1.frame() == instance2.frame()))
-    // {
-    //     auto future = instance1.next();
-    //     auto future2 = instance1.next();
-    //     future.wait();
-    //     future2.wait();
-
-    //     instance2.next();
-    //     wait of
-
-    // }
-
-    // if (instance1.isRunning() and instance2.isRunning())
-    // {
-    //     std::cout << "A difference of exceution was found:" << std::endl;
-    //     std::cout << "Instance " << InstanceNumber1 << " " << instance1.frame() << std::endl;
-    //     std::cout << "instance " << InstanceNumber2 << " " << instance2.frame() << std::endl;
-    // }
-    // else if (instance1.frame() != instance2.frame())
-    // {
-    //     std::cout << "instance " << InstanceNumber1 << (instance1.isRunning() ? " still running" : " has stoped") << std::endl;
-    //     std::cout << "instance " << InstanceNumber2 << (instance1.isRunning() ? " still running" : " has stoped") << std::endl;
-    // }
-    // else
-    // {
-    //     std::cout << "Error occur while run diff command." << std::endl;
-    // }
-
+    if (not framesAreEquals)
+    {
+        std::cout << "A difference of exceution was found:" << std::endl;
+        std::cout << "Instance " << InstanceNumber1 << " " << f1 << std::endl;
+        std::cout << "Instance " << InstanceNumber2 << " " << f2 << std::endl;
+    }
+    else if (not instancesAlive)
+    {
+        std::cout << "Some program has stopped:" << std::endl;
+        std::cout << "Instance " << InstanceNumber1 << (instance1.isAlive() ? " still running" : " has stoped") << std::endl;
+        std::cout << "Instance " << InstanceNumber2 << (instance2.isAlive() ? " still running" : " has stoped") << std::endl;
+    }
+    else
+    {
+        std::cout << "Error occur while run diff command." << std::endl;
+    }
 }
 
 } // namespace NSCommands
